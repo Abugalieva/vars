1ba03ac9b35f48adb94b7c4f3aaf061cdf6c71fb
Anar Abugalieva <26541@almau.edu.kz>
2023-05-18 09:28:49 +0600

    added corsheaders

--- vars/settings.py
+++ vars/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'corsheaders',
     'user',
     'blogs'
 ]
@@ -48,12 +49,19 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     'https://anarina.pythonanywhere.com/'
+# ]
 
 ROOT_URLCONF = 'vars.urls'
 
